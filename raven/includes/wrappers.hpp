7ad0dc8c51ef5a818473229e9e7c88f7123bbc22
Johan511 <harihara.sn@gmail.com>
2024-01-17 10:00:09 +0530

    added move constructors to unique_handler1 and 2

--- raven/includes/wrappers.hpp
+++ raven/includes/wrappers.hpp
@@ -5,9 +5,6 @@
 
 namespace rvn::detail {
 
-template <class T, class... Args>
-std::unique_ptr<T> make_unique(Args &&...);
-
 template <typename... Args>
 QUIC_STATUS NoOpSuccess(Args... args) {
     return QUIC_STATUS_SUCCESS;
@@ -26,11 +23,36 @@ class unique_handler1 {
     Ctor open;
     Dtor close;
 
+    void destroy() {
+        if (handler != NULL) close(handler);
+    }
+
+    void reset() {
+        destroy();
+        handler = NULL;
+    }
+
    protected:
     unique_handler1(Ctor open_, Dtor close_) noexcept
         : handler(NULL) {
         open = open_;
         close = close_;
+    }
+
+    ~unique_handler1() noexcept { destroy(); }
+
+    unique_handler1(const unique_handler1 &) = delete;
+    unique_handler1 &operator=(const unique_handler1 &) = delete;
+
+    unique_handler1(unique_handler1 &&rhs) {
+        reset();
+        // take ownership
+        handler = rhs.handler;
+        open = rhs.open;
+        close = rhs.close;
+
+        // RHS releases ownership
+        rhs.handler = NULL;
     }
 
     /*Don't take universal reference as this is C.
@@ -41,10 +63,6 @@ class unique_handler1 {
     template <typename... Args>
     QUIC_STATUS construct(Args... args) {
         return open(args..., &handler);
-    }
-
-    ~unique_handler1() noexcept {
-        if (handler != NULL) close(handler);
     }
 
     HQUIC get() const { return handler; }
@@ -64,6 +82,18 @@ class unique_handler2 {
     Start start_func;
     Stop stop_func;
 
+    void destroy() {
+        if (handler != NULL) {
+            stop_func(handler);
+            close_func(handler);
+        };
+    }
+
+    void reset() {
+        destroy();
+        handler = NULL;
+    }
+
    protected:
     unique_handler2(Open open_, Close close_, Start start_,
                     Stop stop_ = &NoOpVoid<HQUIC>) noexcept
@@ -73,6 +103,25 @@ class unique_handler2 {
 
         start_func = start_;
         stop_func = stop_;
+    }
+
+    ~unique_handler2() noexcept { destroy(); }
+
+    unique_handler2(const unique_handler2 &) = delete;
+    unique_handler2 &operator=(const unique_handler2 &) = delete;
+
+    unique_handler2(unique_handler2 &&rhs) {
+        reset();
+        // take ownership
+        handler = rhs.handler;
+        open_func = rhs.open_func;
+        close_func = rhs.close_func;
+
+        start_func = rhs.start_func;
+        stop_func = rhs.stop_func;
+
+        // RHS releases ownership
+        rhs.handler = NULL;
     }
 
     /*Don't take universal reference as this is C interface.
@@ -91,11 +140,6 @@ class unique_handler2 {
         status = start_func(handler, args...);
         if (QUIC_FAILED(status)) close_func(handler);
         return status;
-    }
-
-    ~unique_handler2() noexcept {
-        stop_func(handler);
-        close_func(handler);
     }
 
     HQUIC get() const { return handler; }
@@ -118,6 +162,11 @@ class unique_QUIC_API_TABLE : public QUIC_API_TABLE_uptr_t {
    public:
     unique_QUIC_API_TABLE(const QUIC_API_TABLE *tbl);
 };
+
+/* rvn::make_unique should not be called on
+   non specialised template */
+template <class T, class... Args>
+std::unique_ptr<T> make_unique(Args &&...);
 
 static inline unique_QUIC_API_TABLE make_unique() {
     const QUIC_API_TABLE *tbl;
