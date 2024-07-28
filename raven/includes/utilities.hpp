4768c47beeaef377dfa742010f4812a11ab0b31e
Johan511 <harihara.sn@gmail.com>
2024-07-28 20:09:56 +0530

    credentials registered, but throwing error
    Signed-off-by: Johan511 <harihara.sn@gmail.com>

--- raven/includes/utilities.hpp
+++ raven/includes/utilities.hpp
@@ -2,6 +2,8 @@
 
 #include <msquic.h>
 
+#include <iostream>
+#include <ostream>
 #include <utility>
 
 namespace rvn::utils {
@@ -12,4 +14,23 @@ constexpr typename std::underlying_type<E>::type to_underlying(
         e);
 }
 
-}  // namespace rvn::utilities
+template <typename T>
+static void print(std::ostream& os, T value) {
+    os << value << std::endl;
+}
+
+template <typename T, typename... Args>
+static void print(std::ostream& os, T value, Args... args) {
+    os << value << " ";
+    print(os, args...);
+}
+
+template <typename... Args>
+static void ASSERT_LOG_THROW(bool assertion, Args... args) {
+    if (!assertion) {
+        print(std::cerr, args...);
+        throw std::runtime_error("Assertion failed");
+    }
+}
+
+}  // namespace rvn::utils
